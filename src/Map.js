5df29a6866ec45536f7f2fef5245e0c5b74ce35c
elimauskopf <elimauskopf7@gmail.com>
2020-05-27 19:02:44 -0400

    changes from selector to buttons

--- src/Map.js
+++ src/Map.js
@@ -8,8 +8,8 @@ import CanvasDraw from './canvas/index'
 export default class Map extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { 
-			map: split,
+		this.state = {
+			map: haven,
 			color: "#ffc600",
 			brushRadius: 5,
 			lazyRadius: 5,
@@ -29,31 +29,32 @@ export default class Map extends React.Component {
 	}
 
 	handleSelect(e) {
-		console.log(e.target.value)
+	
 		if (e.target.value === 'bind') {
-			this.setState({map: bind})
+			this.setState({ map: bind })
 		} else if (e.target.value === 'split') {
-			this.setState({map: split})
-		}  
+			this.setState({ map: split })
+		}
 		else if (e.target.value === 'haven') {
-			this.setState({map: haven})
+			this.setState({ map: haven })
 		}
 
-		
+		setTimeout(this.drawImage, 100)
+
 	}
+
 
 	updateCanvasDimensions() {
 
-			console.log(window.innerHeight)
-		if(window.innerHeight  <= 600) {
+		if (window.innerHeight <= 600) {
 			this.setState({
 				canvasHeight: 400,
 				canvasWidth: 400
 			})
-		} else if(window.innerHeight  <= 750) {
+		} else if (window.innerHeight <= 750) {
 			this.setState({
 				canvasHeight: 500,
-				canvasWidth: 500 
+				canvasWidth: 500
 			})
 		} else if (window.innerHeight <= 1080) {
 			this.setState({
@@ -73,7 +74,7 @@ export default class Map extends React.Component {
 		}
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		console.log(window.innerHeight)
 		this.updateCanvasDimensions()
 		window.addEventListener('resize', this.updateCanvasDimensions)
@@ -81,16 +82,16 @@ export default class Map extends React.Component {
 
 	colorChanger(e) {
 		if (e.target.className.includes("blue")) {
-			this.setState({color: "#0000ff"})
+			this.setState({ color: "#0000ff" })
 		} else if (e.target.className.includes("green")) {
-			this.setState({color: "#008000"})
+			this.setState({ color: "#008000" })
 		} else if (e.target.className.includes("yellow")) {
-			this.setState({color: "#ffff00"})
+			this.setState({ color: "#ffff00" })
 		} else if (e.target.className.includes("purple")) {
-			this.setState({color: "#800080"})
+			this.setState({ color: "#800080" })
 		}
 
-		if(this.state.erase) {
+		if (this.state.erase) {
 			this.toggleErase()
 		}
 	}
@@ -109,27 +110,22 @@ export default class Map extends React.Component {
 	}
 
 	toggleErase() {
-		if(this.state.erase) {
-			this.setState({erase: false, eraseButtonColor:"", brushRadius: 5})
+		if (this.state.erase) {
+			this.setState({ erase: false, eraseButtonColor: "", brushRadius: 5 })
 		} else {
-			this.setState({erase: true, eraseButtonColor:"Red", brushRadius: 10})
+			this.setState({ erase: true, eraseButtonColor: "Red", brushRadius: 10 })
 		}
 	}
 
 	render() {
 		return (
 			<div>
-				<div className='selector'> 
-					<label htmlFor="cars"> </label>
-
-					<select className='select-css' name="maps" onChange={this.handleSelect}>
-						<option value="split">Split</option> 
-						<option value="bind">Bind</option>
-						<option value="haven">Haven</option>
-					</select>
-					<button className="select-button" onClick={this.drawImage}> Change </button>
+				<div className="selector btn-group">
+					<button value="bind" onClick={this.handleSelect}>Bind</button>
+					<button value="haven" onClick={this.handleSelect}>Haven</button>
+					<button value="split" onClick={this.handleSelect}>Split</button>
 				</div>
-				
+
 				<div className="map">
 					<CanvasDraw
 						ref={canvasDraw => (this.Canvas = canvasDraw)}
@@ -142,15 +138,15 @@ export default class Map extends React.Component {
 						hideInterface={true}
 					/>
 				</div>
-				<div className="buttons"> 
-						<button className="button blue" onClick={this.colorChanger}></button>
-						<button className="button green" onClick={this.colorChanger}></button>
-						<button className="button yellow" onClick={this.colorChanger}></button>
-						<button className="button purple" onClick={this.colorChanger}></button>
-						<button style={{backgroundColor: this.state.eraseButtonColor}} className="reset-button" onClick={this.toggleErase}> Erase </button>
-						<button className="reset-button" onClick={this.clearBoard}> Clear </button>
-						<button className="reset-button" onClick={this.undoLast}> Undo </button>
-					</div>
+				<div className="buttons">
+					<button className="button blue" onClick={this.colorChanger}></button>
+					<button className="button green" onClick={this.colorChanger}></button>
+					<button className="button yellow" onClick={this.colorChanger}></button>
+					<button className="button purple" onClick={this.colorChanger}></button>
+					<button style={{ backgroundColor: this.state.eraseButtonColor }} className="reset-button" onClick={this.toggleErase}> Erase </button>
+					<button className="reset-button" onClick={this.clearBoard}> Clear </button>
+					<button className="reset-button" onClick={this.undoLast}> Undo </button>
+				</div>
 			</div>
 
 		)
